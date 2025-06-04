bd1a9079edbdb930036d34f88087e523690fc17a
trex211 <sasnasuraj@gmail.com>
2025-06-04 16:16:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,7 +7,7 @@ API_ID = int(environ.get("API_ID", "20314087"))
 API_HASH = environ.get("API_HASH", "bb5b852b38f4eae21e83be783348a41c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "AAHLfgoWwE3IBHMVQ_WyfouR0Nyaw8S0Phg")
 OWNER = int(environ.get("OWNER", "5405432233"))
-CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
+CREDIT = "𝄟⃝‌🐬🇳suraj🐬"
 AUTH_USER = os.environ.get('AUTH_USERS', '5405432233').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
